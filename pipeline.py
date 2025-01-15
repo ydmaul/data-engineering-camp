01e85427f8b39e13501b8bb6d41a614ee98071b7
yuda maulana <yuda.maul12@gmail.com>
2025-01-15 15:01:09 +0700

    commit

---
+++ pipeline.py
@@ -0,0 +1,13 @@
+import sys
+import pandas # ini hanya contoh 
+
+#mencetak argumen
+print (sys.argv)
+
+#argumen adalah nama file
+#argumen 1 berisi argumen pertama yang sebenarnya kit aperlukan
+
+day = sys.argv[1]
+
+#tampilkan kalimat dengan argumen
+print{f'job finished successfully for day = {day}'}
